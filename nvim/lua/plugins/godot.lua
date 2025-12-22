fa69683c128e6e22a3ea01ea151b94d95365bff2
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2025-12-22 22:27:44 +0000

    Adding dotfiles for tracking

---
+++ nvim/lua/plugins/godot.lua
@@ -0,0 +1,27 @@
+return {
+  { "habamax/vim-godot" },
+  { "skywind3000/asyncrun.vim" },
+  { "teatek/gdscript-extended-lsp.nvim", opts = { view_type = "floating", picker = "snacks" } },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true, -- show hidden files
+            ignored = false, -- don't show gitignored files
+            exclude = { -- exclude specific patterns
+              "*.uid", -- glob pattern for files ending with .uid
+              "server.pipe", -- exact filename match
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "lommix/godot.nvim",
+    lazy = true,
+    cmd = { "GodotDebug", "GodotBreakAtCursor", "GodotStep", "GodotQuit", "GodotContinue" },
+  },
+}
