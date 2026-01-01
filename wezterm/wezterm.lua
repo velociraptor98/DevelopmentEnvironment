d61bbd0bce14c92b54739dd7dbf3aafdb5a24456
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2026-01-01 22:05:21 +0000

    Remove binding
    Removing the custom bindings, doesn't really work that well across
    windows and mac

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,5 @@ require("events.gui-startup").setup()
 
 return Config:init()
 	:append(require("config.appearance"))
-	:append(require("config.bindings"))
 	:append(require("config.fonts"))
 	:append(require("config.general")).options
