606edf434fe2749b1bbb244857364f0931c375c4
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2026-04-26 01:03:02 +0100

    Update neovim config, fix dap error

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,21 @@
 return {
-  -- Configure LazyVim to load tokyonight
+  -- Add everforest
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme here
+      -- vim.g.everforest_background = "hard"
+      vim.cmd([[colorscheme everforest]])
+    end,
+  },
+
+  -- Configure LazyVim to load everforest
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "everforest",
     },
   },
 }
